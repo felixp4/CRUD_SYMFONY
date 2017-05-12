76abccde16ba93f8290ea45717aa309390a88b18
felix <felixp4@yandex.ru>
2017-05-12 16:37:34 +0300

    Entity Article and Form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,104 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Task;
+use AppBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
+
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+       /**
+        * $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::Class)
+            ->add('dueDate',DateType::Class)
+            ->add('save', SubmitType::Class, array('label'=>'Create post'))
+            ->getForm();
+        */
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+           // 'form'=>$form->CreateView(),
         ]);
+    }
+
+
+    /**
+     * @Route("/show", name="showform")
+     */
+
+    /**
+     public function newAction(Request $request)
+     {
+         $task = new Task();
+         // $task->setTask('Write a blog post');
+         // $task->setDueDate(new \DateTime('tomorrow'));
+
+         $form = $this->createFormBuilder($task)
+             ->add('task', TextType::Class)
+             ->add('dueDate', DateType::Class)
+             ->add('save', SubmitType::Class, array('label' => 'Create post'))
+             ->getForm();
+
+         $form->handleRequest($request);
+
+         if($form->isSubmitted() && $form->isValid()) {
+             $task = $form->getData();
+
+             return $this->redirectToRoute('homepage');
+         }
+
+         return $this->render('default/new.html.twig', array(
+             // 'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+             'form' => $form->CreateView(),
+         ));
+     }
+     *
+     * /
+
+    /**
+     * @Route("/showCRUD", name="showCRUD")
+     */
+
+    public function crudAction(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('name', TextType::Class)
+            ->add('description', TextType::Class)
+            ->add('createdAt', DateType::Class)
+            ->add('save', SubmitType::Class, array('label' => 'Create post object'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->CreateView(),
+        ));
     }
 }
---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,118 @@
+<?php
+// src/AppBundle/Entity/Product.php
+
+Namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\ Entity
+ * @ORM\ Table(name="article")
+ */
+
+class Article
+{
+    /**
+     * @ORM\ Column(type="integer")
+     * @ORM\ Id
+     * @ORM\ GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ Column(type="datetime", name="created_at")
+     */
+    private $createdAt;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param string $price
+     *
+     * @return Product
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
